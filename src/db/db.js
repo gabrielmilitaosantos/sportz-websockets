074ddb592884adb9c1aa48f28e9ff0692f116ccf
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2026-03-05 12:02:09 -0300

    fix some schemas issues

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-serverless";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import ws from "ws";
+// import { Pool, neonConfig } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
+// import ws from "ws";
 
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not defined");
 
-neonConfig.webSocketConstructor = ws;
+// neonConfig.webSocketConstructor = ws;
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool);
